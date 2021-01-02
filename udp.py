98dc1e569250b37f24b35ecfce4f4aa47a7f71c1
Fabian <fabian@eb1tr.com>
2021-01-02 19:27:41 +0100

    Refactor
    Se agregan bandas WARC +60m +6m.
    Refactor para prevenir caídas del código.

--- udp.py
+++ udp.py
@@ -40,78 +40,72 @@ def mqtt_connect():
 
 
 def define_band(qrg):
-    if qrg in range(180000, 200000):
+    if qrg in range(175000, 205000):
         band = 160
-    elif qrg in range(350000, 400000):
+    elif qrg in range(345000, 405000):
         band = 80
-    elif qrg in range(700000, 720000):
+    elif qrg in range(525000, 545000):
+        band = 60
+    elif qrg in range(695000, 730000):
         band = 40
-    elif qrg in range(1400000, 1435000):
+    elif qrg in range(1005000, 1020000):
+        band = 30
+    elif qrg in range(1395000, 1440000):
         band = 20
-    elif qrg in range(2100000, 2145000):
+    elif qrg in range(1805000, 1820000):
+        band = 17
+    elif qrg in range(2095000, 2150000):
         band = 15
-    elif qrg in range(2800000, 2970000):
+    elif qrg in range(2485000, 2500000):
+        band = 12
+    elif qrg in range(2795000, 3000000):
         band = 10
+    elif qrg in range(4995000, 5050000):
+        band = 6
     else:
         band = 0
     return band
 
 
-def publish_radio_info(mqtt_c, radio_i):
-    try:
-        if radio_i[0] == 1:
-            if radio_i[1] == 1:
-                mqtt_c.publish("stn1/radio1/qrg", radio_i[3])
-                mqtt_c.publish("stn1/radio1/band", radio_i[2])
-                mqtt_c.publish("stn1/radio1/mode", radio_i[4])
-                mqtt_c.publish("stn1/radio1/op", radio_i[5])
-            if radio_i[1] == 2:
-                mqtt_c.publish("stn2/radio1/qrg", radio_i[3])
-                mqtt_c.publish("stn2/radio1/band", radio_i[2])
-                mqtt_c.publish("stn2/radio1/mode", radio_i[4])
-                mqtt_c.publish("stn2/radio1/op", radio_i[5])
-        if radio_i[0] == 2:
-            if radio_i[1] == 1:
-                mqtt_c.publish("stn2/radio1/qrg", radio_i[3])
-                mqtt_c.publish("stn2/radio1/band", radio_i[2])
-                mqtt_c.publish("stn2/radio1/mode", radio_i[4])
-                mqtt_c.publish("stn2/radio1/op", radio_i[5])
-            if radio_i[1] == 2:
-                mqtt_c.publish("stn2/radio2/qrg", radio_i[3])
-                mqtt_c.publish("stn2/radio2/band", radio_i[2])
-                mqtt_c.publish("stn2/radio2/mode", radio_i[4])
-                mqtt_c.publish("stn2/radio2/op", radio_i[5])
-    except:
-        print("MQTT problem")
+def publish_data(mqtt_c, radio_i):
+    if radio_i[0] == 1:
+        if radio_i[1] == 1:
+            mqtt_c.publish("stn1/radio1/qrg", radio_i[3])
+            mqtt_c.publish("stn1/radio1/band", radio_i[2])
+            mqtt_c.publish("stn1/radio1/mode", radio_i[4])
+            mqtt_c.publish("stn1/radio1/op", radio_i[5])
+        if radio_i[1] == 2:
+            mqtt_c.publish("stn2/radio1/qrg", radio_i[3])
+            mqtt_c.publish("stn2/radio1/band", radio_i[2])
+            mqtt_c.publish("stn2/radio1/mode", radio_i[4])
+            mqtt_c.publish("stn2/radio1/op", radio_i[5])
+    if radio_i[0] == 2:
+        if radio_i[1] == 1:
+            mqtt_c.publish("stn2/radio1/qrg", radio_i[3])
+            mqtt_c.publish("stn2/radio1/band", radio_i[2])
+            mqtt_c.publish("stn2/radio1/mode", radio_i[4])
+            mqtt_c.publish("stn2/radio1/op", radio_i[5])
+        if radio_i[1] == 2:
+            mqtt_c.publish("stn1/radio1/qrg", radio_i[3])
+            mqtt_c.publish("stn1/radio1/band", radio_i[2])
+            mqtt_c.publish("stn1/radio1/mode", radio_i[4])
+            mqtt_c.publish("stn1/radio1/op", radio_i[5])
 
 
-def process_radio_info(xml_data, mqtt_c):
-    stn = 0
-    radio = int(xml_data["RadioInfo"]['RadioNr'])
-    qrg = int(xml_data["RadioInfo"]['Freq'])
-    band = define_band(qrg)
-    mode = str(xml_data["RadioInfo"]['Mode'])
-    op = str(xml_data["RadioInfo"]['OpCall'])
-    op = op.upper()
-    radio_i = [stn, radio, band, qrg, mode, op]
+def process_xml(xml_data):
+    data = []
+    data.append(0)
+    data.append(int(xml_data["RadioInfo"]['RadioNr']))
+    data.append(define_band(int(xml_data["RadioInfo"]['Freq'])))
+    data.append(int(xml_data["RadioInfo"]['Freq']))
+    data.append(str(xml_data["RadioInfo"]['Mode']))
+    data.append(str(xml_data["RadioInfo"]['OpCall']).upper())
     if xml_data["RadioInfo"]['StationName'] == STN1:
-        radio_i[0] = 1
+        data[0] = 1
     if xml_data["RadioInfo"]['StationName'] == STN2:
-        radio_i[0] = 2
-
-    publish_radio_info(mqtt_c, radio_i)
-
-    if radio_i[0] == 0:
-        print("STN no se ha encontrado: " + str(radio_i))
-    else:
-        print("UDP " + str(radio_i))
-
-
-def process_xml(xml_data, mqtt_c):
-    try:
-        process_radio_info(xml_data, mqtt_c)
-    except:
-        print("Paquete no valido")
+        data[0] = 2
+    print("UDP " + str(data))
+    return data
 
 
 def do_udp():
@@ -123,25 +117,17 @@ def do_udp():
         while True:
             data, address = sock.recvfrom(1024)
             data = data.decode('utf-8')
-            xml_data = xmltodict.parse(data)
-            process_xml(xml_data, mqtt_c)
+            data = xmltodict.parse(data)
+            data = process_xml(data)
+            publish_data(mqtt_c, data)
     except KeyboardInterrupt:
         print('\n** User exited.')
         mqtt_c.disconnect()
         sys.exit(0)
-    except EOFError:
-        print('\n** Closing connection due to EOFError: %s' % EOFError)
-        mqtt_c.disconnect()
-        sys.exit(1)
-    except OSError:
-        print('\n** Closing connection due to OSError: %s' % OSError)
-        mqtt_c.disconnect()
-        sys.exit(1)
     except Exception as e:
-        print('\n** Closing connection due to an exception: %s' % str(e))
-        mqtt_c.disconnect()
-        sys.exit()
+        print('ERR: %s' % str(e))
 
 
 if __name__ == '__main__':
     do_udp()
+
